8938d3b769274908ffbb400a27d9f218bb20c3c9
Will <william.atkinson@connect.qut.edu.au>
2017-12-07 23:19:52 +1000

    Updated Day 2 Part 1 Sol for dynamic tables and started Day 2 Part 2

--- src/days/Day2.java
+++ src/days/Day2.java
@@ -10,10 +10,10 @@ import java.util.Scanner;
 public class Day2 {
 	
 	public static void main(String args[]) {
-		String file = "inputs/day2input";
+		String file = "inputs/day2ex1";
 		Scanner scan = null;
 		List<Integer> list = new ArrayList<Integer>();
-		final int SIZE = 16;
+		int column = 4, row = 3;
 		
 		// scan all values into list of integers
 		try {
@@ -30,20 +30,21 @@ public class Day2 {
 			}
 		}
 		
-		// store all scanned valyes into a jagged array
-		int[][] table = new int[SIZE][SIZE];
-		for(int i = 0; i < SIZE; i++) {
-			for(int j = 0; j < SIZE; j++) {
-				table[i][j] = list.get((i * SIZE) + j).intValue();
+		// store all scanned values into a jagged array
+		int[][] table = new int[row][column];
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < column; j++) {
+				table[i][j] = list.get((i * column) + j).intValue();
 			}
 		} 
 		
+		// PART 1: 
 		// Retrieve all largest and smallest numbers from every row
-		int[][] VIN = new int[16][2];
-		for(int i = 0; i < SIZE; i++) {
+		int[][] VIN = new int[row][2];
+		for(int i = 0; i < row; i++) {
 			int smallest = table[i][0];
 			int largest = table[i][0];
-			for (int j = 0; j < SIZE; j++) {
+			for (int j = 0; j < column; j++) {
 				if (table[i][j] > largest) {
 					largest = table[i][j];
 				} else if (table[i][j] < smallest) {
@@ -54,16 +55,28 @@ public class Day2 {
 			VIN[i][1] = smallest;
 		}
 		
-		// Calculate checksum
+		// Calculate checksum: total of all rows' largest and smallest values subtracted
 		int checksum = 0;
-		for(int i = 0; i < SIZE; i++) {
+		for(int i = 0; i < row; i++) {
 			int large = VIN[i][0];
 			int small = VIN[i][1];
 			checksum += (large - small);
 		}
 		
+		// PART 2:
+		// use the largest number and divide by each other value in array
+		int checksumV2 = 0;
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < column; j++) {
+				int currentNo = table[i][j];
+			}
+		}
+		
 		// DEBUG: Show all largest and smallest numbers of every row
-		System.out.println("Largest and smallest numbers of every row: " + Arrays.deepToString(VIN));
-		System.out.println("Total Checksum: " + checksum);
+		System.out.println("All Values: " + Arrays.deepToString(table));
+//		System.out.println("Largest and smallest numbers of every row: " + Arrays.deepToString(VIN));
+		System.out.println("Total Checksum for Part 1: " + checksum);
+		System.out.println("Total Checksum for Part 2: " + checksumV2);
+		
 	}
 }
