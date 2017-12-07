8af9728dcc317833f8117cc44d7c5acf8a5b68e2
Will <william.atkinson@connect.qut.edu.au>
2017-12-07 13:46:25 +1000

    Day 1 Part 2

--- src/day1/Day1.java
+++ src/day1/Day1.java
@@ -22,31 +22,51 @@ public class Day1 {
 		while(scan.hasNext()) {
 			input = scan.next();
 		} scan.close();
+		int length = input.length();
 		
 		// add individual characters from scan into List<String>
 		List<String> finalInput = new ArrayList<String>();  
-		for (int i = 0; i < input.length(); i++) {
+		for (int i = 0; i < length; i++) {
 			String a_char = Character.toString(input.charAt(i)); 
 			finalInput.add(a_char);
 		}
 		
 		// convert List<String> to integer array
-		int[] intList = new int[input.length()];
-		for (int i = 0; i < input.length(); i++) {
+		int[] intList = new int[length];
+		for (int i = 0; i < length; i++) {
 			intList[i] = Integer.valueOf(finalInput.get(i));
 		}
 		
-		// check conditions and add to final total
+		// PART 1:
+		// check conditions and add to final total (adjacent character is a match)
 		int total = 0;
+		
+//		for (int i = 0; i < length; i++) {
+//			// last number in stream: checks previous and first numbers
+//			if (i == length - 1) {
+//				if (intList[i] == intList[0]) {
+//					total += intList[i];
+//				}
+//			} else {
+//				if (intList[i] == intList[i + 1]) {
+//					total += intList[i];
+//				}
+//			}
+//		}
+		
+		// PART 2:
+		// check conditions and add to final total (characters halfway round is a match)
+		int step = intList.length / 2;
 		for (int i = 0; i < intList.length; i++) {
-			// last number in stream: checks previous and first numbers
-			if (i == intList.length - 1) {
-				if (intList[i] == intList[0]) {
+			// if i is larger than the step
+			if (i >= step) {
+				int temp = intList.length - i;
+				int j = step - temp;
+				if (intList[i] == intList[j]) {
 					total += intList[i];
 				}
-			// every other number
 			} else {
-				if (intList[i] == intList[i + 1]) {
+				if (intList[i] == intList[i + step]) {
 					total += intList[i];
 				}
 			}
